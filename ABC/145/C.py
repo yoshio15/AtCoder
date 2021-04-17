a6f71d03be823834f331f9759dc64071bcbbd375
YoshioKondo <yoshio15310@gmail.com>
2021-04-17 21:45:27 +0900

    C - Average Length

---
+++ ABC/145/C.py
@@ -0,0 +1,22 @@
+import itertools
+import statistics
+import math
+
+N = int(input())
+xy = [list(map(int, input().split())) for l in range(N)]
+
+dis_list = [0] * math.factorial(N)
+i = 0
+for p_tuple in itertools.permutations(xy):
+    dis = 0
+    for k in range(len(p_tuple) - 1):
+        x1 = p_tuple[k][0]
+        x2 = p_tuple[k+1][0]
+        y1 = p_tuple[k][1]
+        y2 = p_tuple[k+1][1]
+        dis += ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+    dis_list[i] = dis
+    i += 1
+
+print(statistics.mean(dis_list))
