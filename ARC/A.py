f6427206e919b5a2086718c07073a72b7b571cc7
YoshioKondo <yoshio15310@gmail.com>
2021-04-18 23:42:28 +0900

    [WIP] A - God Sequence

---
+++ ARC/A.py
@@ -0,0 +1,31 @@
+# 分からん
+A, B = map(int, input().split())
+
+A_list = [0] * A
+B_list = [0] * B
+ans = [0] * (A + B)
+sum_num = 0
+
+if A >= B:
+    for i in range(A):
+        A_list[i] = i + 1
+        sum_num += i + 1
+    for i in range(B):
+        if i == B - 1:
+            B_list[i] = - (sum_num + sum(B_list))
+        else:
+            B_list[i] = - (i + 1)
+    ans = A_list + B_list
+
+else:
+    for i in range(B):
+        sum_num += -(i + 1)
+        B_list[i] = -(i + 1)
+    for i in range(A):
+        if i == A - 1:
+            A_list[i] = - (sum_num - sum(A_list))
+        else:
+            A_list[i] = i + 1
+    ans = A_list + B_list
+
+print(ans)
