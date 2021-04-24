0944cc7d0c49a4b2cefc1e5a23b3fd3d87db20ba
YoshioKondo <yoshio15310@gmail.com>
2021-04-24 20:56:49 +0900

    [WIP] C - 幅優先探索

--- ABC/007/C.py
+++ ABC/007/C.py
@@ -1,4 +1,18 @@
+import collections
+
 R, C = map(int, input().split())
 sy, sx = map(int, input().split())
 gy, gx = map(int, input().split())
-c = [list(input().split()) for _ in range(R)]
+c = [list(input()) for _ in range(R)]
+
+wall = "#"
+way = "."
+
+print(c)
+queue = collections.deque()
+for i in range(R):
+    print("----------")
+    for k in range(C):
+        print(c[i][k])
+        if i == sy - 1 and k == sx - 1:
+            print("Start Point")
