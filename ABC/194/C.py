6275ac420f1d616ad0e1375feec9517394653583
YoshioKondo <yoshio15310@gmail.com>
2021-04-27 23:12:34 +0900

    C - Squared Error

--- ABC/194/C.py
+++ ABC/194/C.py
@@ -1,11 +1,26 @@
 # https://note.com/taraba_kanico/n/n8aca58bcd303
-import itertools
+# import itertools
+#
+# N = int(input())
+# A = list(map(int, input().split()))
+#
+# ans = 0
+# for i in itertools.combinations(A, 2):
+#     ans += (i[0] - i[1]) ** 2
+#
+# print(ans)
 
-N = int(input())
-A = list(map(int, input().split()))
-
+n = int(input())
+a = [i for i in input().split()]
+keys = [i for i in range(-200, 201)]
+values = [0] * 401
+cnt = dict(zip(keys, values))
+for i in range(-200, 201):
+    cnt[i] = a.count(str(i))
 ans = 0
-for i in itertools.combinations(A, 2):
-    ans += (i[0] - i[1]) ** 2
+for i in range(-200, 200):
+    for j in range(i + 1, 201):
+        # print("---i={}, j={}---".format(i, j))
+        ans += cnt[i] * cnt[j] * (i - j) ** 2
 
 print(ans)
