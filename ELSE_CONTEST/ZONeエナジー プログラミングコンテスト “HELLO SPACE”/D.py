7e602c749180a03dad7d9504e4435987314a7706
YoshioKondo <yoshio15310@gmail.com>
2021-05-01 22:58:24 +0900

    宇宙人からのメッセージ

--- ELSE_CONTEST/ZONeエナジー プログラミングコンテスト “HELLO SPACE”/D.py
+++ ELSE_CONTEST/ZONeエナジー プログラミングコンテスト “HELLO SPACE”/D.py
@@ -1,67 +1,24 @@
-s = list(input())
-t = []
-R = "R"
+from collections import deque
 
-is_rd = False
+s = list(input())
+t = deque()
+
+is_rev = False
 for el in s:
-    if el == R:
-        is_rd = not is_rd
-        # t.reverse()
+    if el == "R":
+        is_rev = not is_rev
+    elif is_rev:
+        if t and t[0] == el:
+            t.popleft()
+        else:
+            t.appendleft(el)
     else:
-        if is_rd:
-            t.insert(0, el)
+        if t and t[-1] == el:
+            t.pop()
         else:
             t.append(el)
 
-if is_rd:
+if is_rev:
     t.reverse()
-
-# print("".join(t))
-isDone = False
-while not isDone:
-    bef = ""
-    if len(t) == 0:
-        break
-    for i in range(len(t)):
-        # print("".join(t))
-        if i == 0:
-            bef = t[i]
-            continue
-        if bef == t[i]:
-            del_i = []
-            fir = t[:i]
-            las = t[i:]
-            las.reverse()
-            if len(fir) >= len(las):
-                for k in range(len(las)):
-                    print(del_i)
-                    if fir[k] == las[k]:
-                        del_i.append(i + k)
-                        del_i.append(i - k - 1)
-                        continue
-                    else:
-                        break
-            else:
-                print(t)
-                for k in range(len(fir)):
-                    print(del_i)
-                    if fir[k] == las[k]:
-                        del_i.append(i + k)
-                        del_i.append(i - k - 1)
-                        continue
-                    else:
-                        break
-            del_i = sorted(del_i, reverse=True)
-            print(del_i)
-            for di in del_i:
-                del t[di]
-            break
-
-        else:
-            if i == len(t) - 1:
-                isDone = True
-                break
-            bef = t[i]
-            continue
 
 print("".join(t))
