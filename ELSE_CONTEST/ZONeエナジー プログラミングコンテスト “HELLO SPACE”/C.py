013cd702c5d5999ed4e63276266fbcc07aabfda1
YoshioKondo <yoshio15310@gmail.com>
2021-05-02 23:15:48 +0900

    [WIP] C - MAD TEAM

--- ELSE_CONTEST/ZONeエナジー プログラミングコンテスト “HELLO SPACE”/C.py
+++ ELSE_CONTEST/ZONeエナジー プログラミングコンテスト “HELLO SPACE”/C.py
@@ -1,13 +1,29 @@
-import itertools
-n = int(input())
-abcde = [tuple(map(int, input().split())) for _ in range(n)]
+# 最小値の最大値は二分探索を用いて解く
+N = int(input())
+A = [tuple(map(int, input().split())) for i in range(N)]
 
-ans = 0
-for i in itertools.combinations(abcde, 3):
-    a, b, c, d, e = [list(i) for i in zip(*i)]
-    team_score = [max(a), max(b), max(c), max(d), max(e)]
-    total_score = min(team_score)
-    if total_score > ans:
-        ans = total_score
 
-print(ans)
+def check(x):
+    s = set()
+    for a in A:
+        s.add(sum(1 << i for i in range(5) if a[i] >= x))
+    for x in s:
+        for y in s:
+            for z in s:
+                if x | y | z == 31:
+                    return True
+    return False
+
+
+ok = 0
+ng = 10 ** 9 + 1
+while ng - ok > 1:
+    print("ok={}, ng={}".format(ok, ng))
+    cen = (ok + ng) // 2
+    if check(cen):
+        print("cen={}, Check OK".format(cen))
+        ok = cen
+    else:
+        print("cen={}, Check NG".format(cen))
+        ng = cen
+print(ok)
