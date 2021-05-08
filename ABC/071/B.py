b7a0a679d37eaef79bfecad0bb621e2d5b6844f4
YoshioKondo <yoshio15310@gmail.com>
2021-05-08 17:30:16 +0900

    B - Not Found

---
+++ ABC/071/B.py
@@ -0,0 +1,11 @@
+s = input()
+al = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+      "x", "y", "z"]
+
+ans = "None"
+for el in al:
+    if el not in s:
+        ans = el
+        break
+
+print(ans)
