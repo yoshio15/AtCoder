4bc949c134234b0a80fc92e953efdcff330efa41
YoshioKondo <yoshio15310@gmail.com>
2021-05-16 21:50:18 +0900

    C - Secret Number

--- ABC/201/C.py
+++ ABC/201/C.py
@@ -1,20 +1,20 @@
-import collections
-import math
-
-s = input()
+S = input()
 ans = 0
-cnt = collections.Counter(s)
-
-print(cnt)
-o_cnt = cnt["o"]
-q_cnt = cnt["?"]
-
-pa_a = 1
-pa_b = 14
-pa_c = 36
-pa_d = 72
-
-if o_cnt <= 4 or cnt["x"] == 10:
-    ans = math.factorial(o_cnt)
-
+for i in range(10000):
+    # 0 - 9 の数値の中でどの数値が使われているかを保持
+    flag = [False] * 10
+    now = i
+    for j in range(4):
+        flag[now % 10] = True
+        now //= 10
+    flag2 = True
+    for j in range(10):
+        # o の数値が使われていなかったら NG
+        if S[j] == 'o' and not flag[j]:
+            flag2 = False
+        # x の数値が使われていたら NG
+        if S[j] == 'x' and flag[j]:
+            flag2 = False
+    # bool 型は int 型のサブクラス
+    ans += flag2
 print(ans)
