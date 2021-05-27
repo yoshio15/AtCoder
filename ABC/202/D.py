1c1aa87a9a255b4b6114610449c93cb5b7190584
YoshioKondo <yoshio15310@gmail.com>
2021-05-27 23:20:32 +0900

    D - aab aba baa

--- ABC/202/D.py
+++ ABC/202/D.py
@@ -1,1 +1,21 @@
-a, b, k = map(int, input().split())
+from math import comb
+
+A, B, K = map(int, input().split())
+a = A  # 残っているaの数
+b = B  # 残っているbの数
+ans = ""
+
+# a または b が 0 になると正しく計算できなくなるので、打ち切ります
+# そして余っている文字を全部末尾にくっつけます
+while a > 0 and b > 0:
+    a_comb = comb(a - 1 + b, a - 1)
+    if K <= a_comb:
+        ans += "a"
+        a -= 1
+    else:
+        ans += "b"
+        b -= 1
+        K -= a_comb
+ans += "a" * a
+ans += "b" * b
+print(ans)
