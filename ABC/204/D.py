cd3e07e9e7b42d5272da677ea835ff666491f98f
YoshioKondo <yoshio15310@gmail.com>
2021-06-09 22:29:09 +0900

    D - Cooking

--- ABC/204/D.py
+++ ABC/204/D.py
@@ -1,3 +1,20 @@
 n = int(input())
 t = list(map(int, input().split()))
-print(t)
+s = sum(t)
+
+dp = [[False] * (s + 1) for _ in range(n + 1)]
+dp[0][0] = True
+
+for i in range(n):
+    for j in range(s + 1):
+        if dp[i][j]:
+            dp[i + 1][j] = True
+        if j - t[i] >= 0 and dp[i][j - t[i]]:
+            dp[i + 1][j] = True
+
+ans = 10 ** 10
+for i in range(s + 1):
+    if dp[n][i]:
+        score = max(i, s - i)
+        ans = min(ans, score)
+print(ans)
