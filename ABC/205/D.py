263694c235119f8f6176b11c968f6273946e12fa
YoshioKondo <yoshio15310@gmail.com>
2021-06-13 23:11:50 +0900

    [WIP] D - Kth Excluded

---
+++ ABC/205/D.py
@@ -0,0 +1,8 @@
+n, q = map(int, input().split())
+a = list(map(int, input().split()))
+ans_list = []
+for i in range(q):
+    k = int(input())
+    ans_list.append(k)
+for ans in ans_list:
+    print(ans)
