7af79c11ff00fd5a2efb7518e01ed992f1267abe
YoshioKondo <yoshio15310@gmail.com>
2021-06-14 20:17:53 +0900

    C - Kaprekar Number

--- ABC/192/C.py
+++ ABC/192/C.py
@@ -1,44 +1,16 @@
-n, k = map(int, input().split())
-
-test = ["0", "0", "2", "4"]
-print(int("".join(test)))
+def g1(n):
+    return int(''.join(sorted(str(n))[::-1]))
 
 
-def g2(x):
-    arr = []
-    for el in list(str(x)):
-        arr.append(int(el))
-    arr.sort()
-    arr_str = []
-    for el in arr:
-        arr_str.append(str(el))
-    return int("".join(arr_str))
+def g2(n):
+    return int(''.join(sorted(str(n))))
 
 
-def g1(x):
-    arr = []
-    for el in list(str(x)):
-        arr.append(int(el))
-    arr.sort(reverse=True)
-    arr_str = []
-    for el in arr:
-        arr_str.append(str(el))
-    return int("".join(arr_str))
+def f(n):
+    return g1(n) - g2(n)
 
 
-def f(x):
-    return g1(x) - g2(x)
-
-
-ans = 0
-ans_list = []
-tar = n
-for i in range(k):
-    tmp = f(tar)
-    ans_list.append(tmp)
-    ans = tmp
-    if tmp == tar:
-        break
-    tar = tmp
-
-print(ans)
+N, K = map(int, input().split())
+for _ in range(K):
+    N = f(N)
+print(N)
