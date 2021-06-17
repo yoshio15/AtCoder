4a1140fc645640f148538b7ac6aed251fcafacb6
YoshioKondo <yoshio15310@gmail.com>
2021-06-17 20:02:25 +0900

    C - Super Ryuma

--- ABC/184/C.py
+++ ABC/184/C.py
@@ -1,11 +1,21 @@
 r1, c1 = map(int, input().split())
 r2, c2 = map(int, input().split())
 ans = 0
-while True:
-    if r1 + c1 == r2 + c2 \
-            or r1 - c1 == r2 - c2 \
-            or abs(r1 - r2) + abs(c1 - c2) <= 3:
-        break
-    # TODO 最適なところに r1, c1 を動かす
-    ans += 1
+# 3手あればどの箇所へでも移動可能
+r = r2 - r1
+c = c2 - c1
+if (r, c) == (0, 0):
+    ans = 0
+elif r == c or r == -c:
+    ans = 1
+elif abs(r) + abs(c) <= 3:
+    ans = 1
+elif (r ^ c ^ 1) & 1:
+    ans = 2
+elif abs(r) + abs(c) <= 6:
+    ans = 2
+elif abs(r + c) <= 3 or abs(r - c) <= 3:
+    ans = 2
+else:
+    ans = 3
 print(ans)
