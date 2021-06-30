1dee60bd0e3e7aec356fa4197d9c82f3ed363346
YoshioKondo <yoshio15310@gmail.com>
2021-06-30 23:34:49 +0900

    C - Cream puff

---
+++ ABC/180/C.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+
+def make_divisors(n):
+    lower_divisors, upper_divisors = [], []
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n // i)
+        i += 1
+    return lower_divisors + upper_divisors[::-1]
+
+
+ans_list = make_divisors(n)
+ans_list.sort()
+for el in ans_list:
+    print(el)
