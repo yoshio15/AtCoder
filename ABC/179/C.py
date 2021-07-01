be2447b801c773c58078befc35cc5857b406c26f
YoshioKondo <yoshio15310@gmail.com>
2021-07-01 23:37:31 +0900

    C - A x B + C

--- ABC/179/C.py
+++ ABC/179/C.py
@@ -1,12 +1,5 @@
 n = int(input())
-ans_list = []
 ans = 0
-for c in range(1, n):
-    tar = n - c
-    for a in range(1, tar + 1):
-        if tar % a == 0:
-            # tmp = [a, tar // a, c]
-            # ans_list.append(tmp)
-            ans += 1
-# print(ans_list)
+for a in range(1, n):
+    ans += (n - 1) // a
 print(ans)
