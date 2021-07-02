63054d3209553599d6ee33fb8569baa8edd64168
YoshioKondo <yoshio15310@gmail.com>
2021-07-02 23:14:11 +0900

    C - Ubiquity

--- ABC/178/C.py
+++ ABC/178/C.py
@@ -1,14 +1,4 @@
-import math
-
-
-def comb(n, r):
-    return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
-
-
-N = int(input())
+n = int(input())
 ans = 0
 mod = 10 ** 9 + 7
-if N > 1:
-    ans = comb(N, 2) * 2 * (10 ** (N - 2))
-
-print(ans % mod)
+print((10 ** n - 9 ** n - 9 ** n + 8 ** n) % mod)
