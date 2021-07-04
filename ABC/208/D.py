0e53c913af58b8c4566f4bb0eb4f3edbf072a3f7
YoshioKondo <yoshio15310@gmail.com>
2021-07-04 23:38:25 +0900

    [WIP] D - Shortest Path Queries 2

---
+++ ABC/208/D.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+abc = [list(map(int, input().split())) for _ in range(m)]
+tot = 0
+abc = dict()
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    abc[a] =
+
+print(abc)
