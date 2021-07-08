73c8e1c2de6c2c527c3fbd6528d3ca1d16d8af71
YoshioKondo <yoshio15310@gmail.com>
2021-07-08 23:44:57 +0900

    [WIP] C - H and V

--- ABC/173/C.py
+++ ABC/173/C.py
@@ -2,7 +2,6 @@ h, w, k = map(int, input().split())
 c = [list(input()) for _ in range(h)]
 cnt = 0
 for i in range(2 ** h):
-    print("-----------------------------")
     rows = []
     for j in range(h):
         if (i >> j) & 1:
@@ -12,9 +11,7 @@ for i in range(2 ** h):
         for k in range(w):
             if (j >> k) & 1:
                 cols.append(k + 1)
-        print("-----")
-        print("選択行: {}".format(rows))
-        print("選択列: {}".format(cols))
+        print("選択行: {}, 選択列: {}".format(rows, cols))
         # TODO: 選択行・列以外の黒の数をカウントする
         # 黒の数が K と等しかった場合、カウントアップしていく
 print(cnt)
