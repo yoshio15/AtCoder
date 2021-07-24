120c289e598231ad052c97cac550e130cbfb0798
Yoshio Kondo <yoskondo@yahoo-corp.jp>
2021-07-25 00:58:46 +0900

    C - chokudai

--- ABC/211/C.py
+++ ABC/211/C.py
@@ -1,24 +1,21 @@
-import collections
-s = list(input())
+CHOKUDAI = 'chokudai'
+s = input()
 mod = 10 ** 9 + 7
-ans = 0
-cnt = collections.Counter(s)
-print(cnt)
-for i in range(len(s)):
-    if s[i] == 'c':
-        for j in range(i + 1, len(s)):
-            if s[j] == 'h':
-                for k in range(j + 1, len(s)):
-                    if s[k] == 'o':
-                        for l in range(k + 1, len(s)):
-                            if s[l] == 'k':
-                                for m in range(l + 1, len(s)):
-                                    if s[m] == 'u':
-                                        for n in range(m + 1, len(s)):
-                                            if s[n] == 'd':
-                                                for o in range(n + 1, len(s)):
-                                                    if s[o] == 'a':
-                                                        for p in range(o + 1, len(s)):
-                                                            if s[p] == 'i':
-                                                                ans += 1
-print(ans % mod)
+dp = [[0] * (len(s) + 1) for _ in range(len(CHOKUDAI))]
+for i in range(len(CHOKUDAI)):
+    for j in range(len(s) + 1):
+        if j == 0 or j > len(s):
+            continue
+        if i == 0:
+            if s[j - 1] == CHOKUDAI[i]:
+                dp[i][j] = dp[i][j - 1] + 1
+            else:
+                dp[i][j] = dp[i][j - 1]
+        else:
+            if s[j - 1] == CHOKUDAI[i]:
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1]
+            else:
+                dp[i][j] = dp[i][j - 1]
+# for el in dp:
+#     print(el)
+print(dp[len(CHOKUDAI) - 1][len(s)] % mod)
